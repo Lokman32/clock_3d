366f1ecbc2cec2826eeb690663f95b25ed696a0f
Louqmane <ll1330137@gmail.com>
2024-12-02 09:13:56 +0100

    evolution 1

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,17 @@ function BasicRing({ scale, speed = 0 }) {
 
   return (
     <mesh ref={ringRef} scale={scale}>
-      <torusGeometry args={[1, 0.05, 5, 12]} />
-      <meshStandardMaterial color="gold" side={2} wireframe />
+      <torusGeometry args={[1, 0.05, 100, 100]} />
+      <meshStandardMaterial color="gold" side={2} />
     </mesh>
   );
 }
 
-function ClockWise({ color, position,scale,speed }) {
+function ClockWise({ color, position, scale, speed }) {
   const wise = useRef();
 
   useFrame((state) => {
-    const elapsedTime = state.clock.getElapsedTime() / (60**speed);
+    const elapsedTime = state.clock.getElapsedTime() / 60 ** speed;
     wise.current.rotation.z = -elapsedTime * 2 * Math.PI;
   });
 
@@ -32,7 +32,7 @@ function ClockWise({ color, position,scale,speed }) {
     <group ref={wise}>
       <mesh position={position} scale={scale}>
         <boxGeometry args={[0.1, 0.8, 0.01]} />
-        <meshStandardMaterial color={color} wireframe />
+        <meshStandardMaterial color={color} />
       </mesh>
     </group>
   );
@@ -40,17 +40,27 @@ function ClockWise({ color, position,scale,speed }) {
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [0, 0, 6], fov: 50 }}>
+    <Canvas camera={{ position: [0, 0, 6], fov: 50, far:.01, away:1000 }}>
       <ambientLight intensity={0.5} />
       <pointLight position={[5, 5, 5]} />
 
-      <group rotation-y={Math.PI * 0.15}>
+      <group /* rotation-y={Math.PI * 0.15} */>
         <BasicRing scale={1.575} speed={-0.01} />
         <BasicRing scale={1.4} speed={0.01} />
         <BasicRing scale={1} />
         <ClockWise scale={1} speed={1} color="red" position={[0, 0.4, 0]} />
-        <ClockWise scale={.9} speed={2} color="green" position={[0, 0.36, 0.01]} />
-        <ClockWise scale={.7} speed={3} color="blue" position={[0, 0.28, 0.02]} />
+        <ClockWise
+          scale={0.9}
+          speed={2}
+          color="green"
+          position={[0, 0.36, 0.01]}
+        />
+        <ClockWise
+          scale={0.7}
+          speed={3}
+          color="blue"
+          position={[0, 0.28, 0.02]}
+        />
       </group>
 
       <OrbitControls />
